485cbee150df3859f6bfcf4805407cbc00f810ac
Nishanth Salinamakki <nrs3@illinois.edu>
2020-09-30 01:18:18 -0700

    Added CORS support

--- covidsearch_backend/covidsearch_backend/settings.py
+++ covidsearch_backend/covidsearch_backend/settings.py
@@ -52,7 +52,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = ["localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "covidsearch_backend.urls"
 

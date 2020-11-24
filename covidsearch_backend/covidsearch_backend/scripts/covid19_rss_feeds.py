0a62b32f2bde4c59055d69131af2209751650fad
Nishanth Salinamakki <nrs3@illinois.edu>
2020-11-23 19:49:47 -0800

    Finished news article scraper; works pretty well on a majority of articles (didn't work on ~30 articles)

--- covidsearch_backend/covidsearch_backend/scripts/covid19_rss_feeds.py
+++ covidsearch_backend/covidsearch_backend/scripts/covid19_rss_feeds.py
@@ -2,7 +2,6 @@
 
 from bs4 import BeautifulSoup
 import feedparser
-import html2text
 import html5lib
 import json
 import os
@@ -20,43 +19,38 @@ FEEDS = {
     "mit_technology_review_rss": "https://cdn.technologyreview.com/stories.rss",
 }
 
-
-def _clean_webpage_html(file_dir: str, local_file: str, clean_file: str) -> None:
-    html_parser = html2text.HTML2Text()
-    html_parser.ignore_links = True
-    with open(local_file, "r") as uncleaned_webpage:
-        cleaned_webpage_text = html_parser.handle(uncleaned_webpage.read())
-    os.remove(local_file)
-
-    feed_dir = file_dir
-    if not os.path.exists(feed_dir):
-        os.makedirs(feed_dir)
-    webpage_path = f"{feed_dir}/{clean_file}"
-    with open(webpage_path, "w+") as cleaned_webpage:
-        cleaned_webpage.write(cleaned_webpage_text)
+FEEDS_SCRAPE_TAG = {
+    "nyt_health": "StoryBodyCompanionColumn",
+    "nyt_science": "StoryBodyCompanionColumn",
+    "nyt_travel": "StoryBodyCompanionColumn",
+    "wsj_lifestyle": "article-content",
+    "wsj_world_news": "article-content",
+    "wired_rss": "article__chunks",
+    "mit_technology_review_rss": "storyContent",
+}
 
 
-def _scrape_article_text(feed_title: str, page_title: str, page_url: str) -> str:
-    """
-    webpage_uncleaned_html = requests.get(page_url).text
-    with open(page_title, "w+") as webpage_file:
-        webpage_file.write(webpage_uncleaned_html)
-        _clean_webpage_html(feed_title, page_title, f"final_{page_title}.txt")
-    """
-
+def _scrape_article_text(
+    feed_title: str, feed_scrape_tag: str, page_title: str, page_url: str
+) -> str:
     article = requests.get(page_url)
     article_content = article.content
     soup_article = BeautifulSoup(article_content, "html5lib")
-    article_bodies = soup_article.find_all("div", class_=re.compile("storyContent"))
+    article_bodies = soup_article.find_all("div", class_=re.compile(feed_scrape_tag))
     if not article_bodies:
+        print(f"Could not find article body for {page_title} at {page_url}")
         return ""
 
-    article_body = article_bodies[0]
-    article_text = article_body.find_all("p")
-    with open(f"{page_title}.txt", "w+") as webpage_file:
-        for article_paragraph in article_text:
-            article_paragraph_text = article_paragraph.get_text()
-            webpage_file.write(f"{article_paragraph_text}\n")
+    if not os.path.exists(feed_title):
+        os.makedirs(feed_title)
+    page_title_filename = page_title.replace(" ", "_")
+    webpage_path = f"{feed_title}/{page_title_filename}.txt"
+    with open(webpage_path, "w+") as webpage_file:
+        for article_body in article_bodies:
+            body_text = article_body.find_all("p")
+            for paragraph in body_text:
+                paragraph_text = paragraph.get_text()
+                webpage_file.write(f"{paragraph_text}\n")
 
     return ""
 
@@ -71,18 +65,19 @@ def parse_and_upload_rss_feed_data(feed_data_filename: str):
             feed_data_file.write(f"Feed: {feed}\n")
             feed_data_file.write(json.dumps(rss_parsed, indent=4))
 
+            feed_scrape_tag = FEEDS_SCRAPE_TAG[feed]
             feed_metadata = rss_parsed["feed"]
             feed_title = feed_metadata["title"]
             feed_entries = rss_parsed["entries"]
             if not feed_entries:
                 continue
 
-            for feed_entry in feed_entries[0:1]:
+            for feed_entry in feed_entries:
                 article_title = feed_entry["title"]
-                print(f"Article title: {article_title}")
                 article_url = feed_entry["link"]
-                print(f"Article url: {article_url}")
-                _scrape_article_text(feed_title, article_title, article_url)
+                _scrape_article_text(
+                    feed_title, feed_scrape_tag, article_title, article_url
+                )
 
 
 def main():
